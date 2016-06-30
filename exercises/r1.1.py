64a1c36e0dd50f57eaa0b0fd975ef0cee67570cb
krnets <cmantheo@gmail.com>
2016-06-30 14:19:06 +1000

    first commit

---
+++ exercises/r1.1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+R-1.1 Write a short Python function, is multiple(n, m), that takes two integer
+values and returns True if n is a multiple of m, that is, n = mi for some
+integer i, and False otherwise.
+'''
+
+def is_multiple(n, m):
+    return n % m == 0
+
+
+if __name__ == "__main__":
+    a, b = input("Enter two whole numbers separated by space: ").split()
+    print("is " + a + " multiple of " + b + "?\n", is_multiple(int(a), int(b)))
